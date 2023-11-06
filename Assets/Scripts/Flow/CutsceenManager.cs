a2304bc46f0406f9cf5962f731a505b380e704c1
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2023-11-06 18:59:12 +0300

    cutsceens

--- Assets/Scripts/Flow/CutsceenManager.cs
+++ Assets/Scripts/Flow/CutsceenManager.cs
@@ -1,5 +1,6 @@
 using Enemy;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 namespace GameFlow
@@ -9,15 +10,25 @@ namespace GameFlow
         [SerializeField] private List<Cutsceen> _cutsceens;
         private int _cutsceenIndex;
 
-        private void Start()
+        private IEnumerator Start()
         {
+            yield return new WaitForSeconds(.001f);
             _cutsceenIndex = 0;
+            StartCutscene();
             EventBusSingleton.Instance.Subscribe<LevelComplete>(OnLevelComplete);    
         }
 
         private void OnDestroy()
         {
             EventBusSingleton.Instance.Unsubscribe<LevelComplete>(OnLevelComplete);
+        }
+
+        private void StartCutscene()
+        {
+            Level firstLevel = FindObjectOfType<Level>();
+
+            StartCoroutine(_cutsceens[_cutsceenIndex].StartCutsceen(firstLevel._levelSpawner));
+            _cutsceenIndex++;
         }
 
         private void OnLevelComplete(LevelComplete signal)
